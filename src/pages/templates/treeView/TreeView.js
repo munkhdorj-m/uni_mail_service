72a7d836e5ad74e27c7ee49e6a9a6bcf1928c251
M-Munkhdorj <78461257+M-Munkhdorj@users.noreply.github.com>
2023-01-16 16:28:05 +0800

    init

---
+++ src/pages/templates/treeView/TreeView.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+
+import TreeView from "devextreme-react/tree-view";
+import "./styles.css";
+
+function App() {
+  const [items, setItems] = useState();
+
+  const value = "contains";
+  const url = "http://127.0.0.1:8080/mail/template-types";
+
+  const fetchToServer = async (data) => {
+    const url = "http://127.0.0.1:8080/mail/template";
+    let toSend = { name: trimmedData };
+    console.log(trimmedData);
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(toSend),
+    })
+      .then((response) => response.json())
+      .then((data) => alert(data.msg));
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
+  // const itemTemplate = (item) => {
+  //   if (item.price) {
+  //     return `${item.name} ($${item.price})`;
+  //   } else {
+  //     return `${item.name}`;
+  //   }
+  // };
+
+  const itemClick = (e) => {
+    fetchToServer(e.e.itemData.name);
+    console.log(e.itemData.name);
+  };
+
+  return (
+    <TreeView
+      id="treeview"
+      dataSource={items}
+      displayExpr="name"
+      onItemClick={itemClick}
+      parentIdExpr="categoryId"
+      width={300}
+      searchMode={value}
+      searchEnabled={true}
+    />
+  );
+}
+
+export { App as TemplatesTreeView };
