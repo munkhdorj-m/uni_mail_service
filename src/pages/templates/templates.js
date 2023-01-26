bc37f30783c1863d9c8b86df1ceccf1d0e29f24b
M-Munkhdorj <78461257+M-Munkhdorj@users.noreply.github.com>
2023-01-26 14:59:31 +0800

    Up

--- src/pages/templates/templates.js
+++ src/pages/templates/templates.js
@@ -4,15 +4,8 @@ import Menu from "devextreme-react/menu";
 import TileView from "devextreme-react/tile-view";
 import "./style.css";
 
-import sample from "./sample2.json";
+import sample from "./design.json";
 import EmailEditor from "react-email-editor";
-import HtmlEditor, {
-  Toolbar,
-  MediaResizing,
-  ImageUpload,
-  Item,
-} from "devextreme-react/html-editor";
-
 import Form, {
   SimpleItem,
   EmailRule,
@@ -23,7 +16,6 @@ import notify from "devextreme/ui/notify";
 import "devextreme-react/autocomplete";
 import "./style.css";
 
-import { markup } from "./data.js";
 import { Button } from "devextreme-react";
 
 class Templates extends React.Component {
@@ -43,7 +35,7 @@ class Templates extends React.Component {
 
     this.myRef = React.createRef();
     this.buttonOptions = {
-      onClick: this.sendEmail,
+      onClick: this.Cons,
       text: "Send Email",
       type: "success",
       useSubmitBehavior: true,
@@ -51,33 +43,36 @@ class Templates extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // sizeValues = ["8pt", "10pt", "12pt", "14pt", "18pt", "24pt", "36pt"];
-  // fontValues = [
-  //   "Arial",
-  //   "Courier New",
-  //   "Georgia",
-  //   "Impact",
-  //   "Lucida Console",
-  //   "Tahoma",
-  //   "Times New Roman",
-  //   "Verdana",
-  // ];
-  // headerValues = [false, 1, 2, 3, 4, 5];
+  async componentDidMount() {
+    try {
+      const responseFromTemplateTypes = await fetch(
+        "http://127.0.0.1:8080/mail/template-types"
+      );
+      const dataOfTemplateTypes = await responseFromTemplateTypes.json();
+      const responseFromTemplate = await fetch(
+        "http://127.0.0.1:8080/mail/template"
+      );
+      const dataOfTemplate = await responseFromTemplate.json();
+      this.setState({ menuItems: dataOfTemplateTypes });
+      this.setState({ templates: dataOfTemplate });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  createTemplate = async (data) => {
+  createTemplateInServer = async (data) => {
     const url = "http://127.0.0.1:8080/mail/template";
     let toSend = {};
     const jsonstring = JSON.stringify(data);
     toSend.html = jsonstring;
-
-    console.log(jsonstring);
+    toSend.image_src = this.state.tileViewItemData.image_src;
+    console.log(this.state.tileViewItemData);
     fetch(url, {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify(toSend),
     });
@@ -88,18 +83,28 @@ class Templates extends React.Component {
     // });
   };
 
-  getTemplateTypes = async () => {
-    const url = "http://127.0.0.1:8080/mail/template-types";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => {
-        console.error(error);
-      });
+  deleteTemplateInServer = async (data) => {
+    const url = "http://127.0.0.1:8080/mail/template";
+    let toSend = {};
+    const jsonstring = JSON.stringify(data);
+    toSend.html = jsonstring;
+    toSend.id = this.state.tileViewItemData.id;
+
+    fetch(url, {
+      method: "DELETE",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toSend),
+    });
   };
 
-  updateTemplate = async () => {
-    let toSend = { html: this.state.valueContent };
+  updateTemplateInServer = async (data) => {
+    let toSend = {};
+    const jsonstring = JSON.stringify(data);
+    toSend.html = jsonstring;
     toSend.id = this.state.tileViewItemData.id;
     console.log(toSend);
     const url = "http://127.0.0.1:8080/mail/template";
@@ -146,30 +151,6 @@ class Templates extends React.Component {
       });
   };
 
-  editorValueChanged = (e) => {
-    this.setState({ valueContent: e.value });
-    // console.log(valueContent);
-  };
-
-  handleSubmit(e) {
-    notify(
-      {
-        message: "Email has sent successfully",
-        position: {
-          my: "center top",
-          at: "center top",
-        },
-      },
-      "success",
-      3000
-    );
-    e.preventDefault();
-  }
-
-  createNewTemplate = (e) => {
-    this.createTemplate(this.state.valueContent);
-  };
-
   getTemplateDataFromServer = async (data) => {
     const url = "http://127.0.0.1:8080/mail/get-template-types";
     let toSend = { name: data };
@@ -190,22 +171,36 @@ class Templates extends React.Component {
       });
   };
 
-  async componentDidMount() {
-    try {
-      const responseFromTemplateTypes = await fetch(
-        "http://127.0.0.1:8080/mail/template-types"
-      );
-      const dataOfTemplateTypes = await responseFromTemplateTypes.json();
-      const responseFromTemplate = await fetch(
-        "http://127.0.0.1:8080/mail/template"
-      );
-      const dataOfTemplate = await responseFromTemplate.json();
-      this.setState({ menuItems: dataOfTemplateTypes });
-      this.setState({ templates: dataOfTemplate });
-    } catch (error) {
-      console.error(error);
-    }
+  editorValueChanged = (e) => {
+    this.setState({ valueContent: e.value });
+  };
+
+  handleSubmit(e) {
+    notify(
+      {
+        message: "Email has sent successfully",
+        position: {
+          my: "center top",
+          at: "center top",
+        },
+      },
+      "success",
+      3000
+    );
+    e.preventDefault();
   }
+
+  deleteTemplate = (e) => {
+    this.deleteTemplateInServer(this.state.valueContent);
+  };
+
+  createNewTemplate = (e) => {
+    this.createTemplateInServer(this.state.valueContent);
+  };
+
+  updateTemplate = (e) => {
+    this.updateTemplateInServer(this.state.valueContent);
+  };
 
   menuItemClick = (e) => {
     this.getTemplateDataFromServer(e.itemData.name);
@@ -213,41 +208,39 @@ class Templates extends React.Component {
   };
 
   tileViewItemClick = (e) => {
-    this.setState({ tileViewItemData: e.itemData });
-    console.log(e.itemData);
+    this.state.tileViewItemData = e.itemData;
+    console.log(this.state.tileViewItemData);
+    this.changeTemplate(e.itemData);
   };
 
   saveDesign = () => {
     this.myRef.current.editor.saveDesign((design) => {
-      // console.log("saveDesign- ", JSON.stringify(design));
+      console.log(design);
+      this.state.valueContent = design;
     });
   };
 
+  changeTemplate = (data) => {
+    const jsonString = JSON.parse(data.html);
+    this.myRef.current.editor.loadDesign(jsonString);
+  };
+
   onLoad = () => {
-    this.myRef.current.editor.loadDesign(sample);
+    // this.myRef.current.editor.loadDesign(sample);
     this.myRef.current.editor.addEventListener(
       "design:updated",
       this.saveDesign
     );
   };
+
   onDesignLoad = (data) => {
     console.log("onDesignLoad", data);
   };
 
   Cons = async () => {
     await this.myRef.current.editor.exportHtml(async (data) => {
-      const { html } = data;
-      console.log(html);
-      this.setState({ valueContent: data });
       this.state.valueContent = data.html;
-      console.log(this.state.valueContent);
       this.sendEmail();
-      // const response = await axios.post('http://localhost:4000/send-email', {mensaje: html},{
-      //   headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
-      // }).catch((error)=>{
-      //   console.log(error);
-      // });
-      // console.log(response);
     });
   };
   render() {
@@ -264,11 +257,11 @@ class Templates extends React.Component {
             id="tileView"
             onItemClick={this.tileViewItemClick}
             items={this.state.templates}
-            height={"450px"}
+            height={"500px"}
             direction={"horizontal"}
             baseItemHeight={400}
             baseItemWidth={230}
-            showScrollbar={"onHover"}
+            showScrollbar={"always"}
             itemMargin={10}
             itemComponent={TileViewItem}
           />
@@ -277,56 +270,10 @@ class Templates extends React.Component {
             onLoad={this.onLoad}
             onDesignLoad={this.onDesignLoad}
           />
-          {/* <HtmlEditor
-              height="725px"
-              // value={this.state.valueContent}
-              value={this.state.tileViewItemData.html}
-              valueType="html"
-              onValueChanged={this.editorValueChanged}
-            >
-              <MediaResizing enabled={true} />
-              <ImageUpload
-                fileUploadMode="base64"
-                tabs="file"
-                uploadUrl="http://127.0.0.1:8080/mail/upload"
-              />
-              <Toolbar multiline="true">
-                <Item name="undo" />
-                <Item name="redo" />
-                <Item name="separator" />
-                <Item name="size" acceptedValues={this.sizeValues} />
-                <Item name="font" acceptedValues={this.fontValues} />
-                <Item name="separator" />
-                <Item name="bold" />
-                <Item name="italic" />
-                <Item name="strike" />
-                <Item name="underline" />
-                <Item name="separator" />
-                <Item name="alignLeft" />
-                <Item name="alignCenter" />
-                <Item name="alignRight" />
-                <Item name="alignJustify" />
-                <Item name="separator" />
-                <Item name="orderedList" />
-                <Item name="bulletList" />
-                <Item name="separator" />
-                <Item name="header" acceptedValues={this.headerValues} />
-                <Item name="separator" />
-                <Item name="color" />
-                <Item name="background" />
-                <Item name="separator" />
-                <Item name="link" />
-                <Item name="image" />
-                <Item name="separator" />
-                <Item name="clear" />
-                <Item name="separator" />
-              </Toolbar>
-            </HtmlEditor> */}
-
           <div className="options">
             <form onSubmit={this.handleSubmit}>
               <Form
-                colCount={3}
+                colCount={2}
                 formData={this.state.emailInfo}
                 readOnly={false}
                 showColonAfterLabel={true}
@@ -336,10 +283,10 @@ class Templates extends React.Component {
                   <RequiredRule message="Email is required" />
                   <EmailRule message="Email is invalid" />
                 </SimpleItem>
-                <SimpleItem dataField="Sender" editorType="dxTextBox">
+                {/* <SimpleItem dataField="Sender" editorType="dxTextBox">
                   <RequiredRule message="Email is required" />
                   <EmailRule message="Email is invalid" />
-                </SimpleItem>
+                </SimpleItem> */}
                 <SimpleItem dataField="Subject">
                   <RequiredRule message="Subject is required" />
                 </SimpleItem>
@@ -362,8 +309,8 @@ class Templates extends React.Component {
             />
             <Button
               className="button-item"
-              text="get"
-              onClick={this.getTemplateTypes}
+              text="delete template"
+              // onClick={this.deleteTemplate}
             />
           </div>
         </div>
@@ -375,15 +322,13 @@ class Templates extends React.Component {
 function TileViewItem({ data }) {
   return (
     <div className="dx-tile-content">
-      <div className="templateName">{"Template " + data.id}</div>
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${data.ImageSrc})`,
-        }}
-      >
-        {data.html}
-      </div>
+      {/* <div className="templateName">{"Template " + data.id}</div> */}
+      <img
+        className="tile-image"
+        src={data.image_src}
+        max-width={230}
+        height={400}
+      />
     </div>
   );
 }
